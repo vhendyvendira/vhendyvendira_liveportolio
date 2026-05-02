2a57d2c20c7aaaa6921a770e906425ee5fda0503
VNDY <Vhendypersonal@gmail.com>
2026-05-02 21:11:27 +0700

    fix: Update CV link in About section
    The previous link to the CV in the About section was broken. This commit updates the link to a valid Google Drive URL.

--- src/components/AboutView.tsx
+++ src/components/AboutView.tsx
@@ -547,7 +547,7 @@ export default function AboutView({ navigate }: AboutViewProps) {
             
             <div style={{ display: 'flex', flexDirection: 'column', gap: '0.5rem' }}>
               <a 
-                href="https://drive.google.com/file/d/1n7QADZR_P1iGWJFb2-FYH4X-tDGFix3Y/view?usp=sharing" 
+                href="https://drive.google.com/file/d/1dzD2RoKC7XdhaWGlXEnSwCzk1T35rYYY/view?usp=sharing" 
                 target="_blank"
                 rel="noopener noreferrer"
                 className="about-cta-link" 
